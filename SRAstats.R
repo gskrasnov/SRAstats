53c952cd93dbb61178605285da686e68b95b0dbe
gskrasnov <gskrasnov@mail.ru>
2021-12-24 13:34:14 +0300

    Add files via upload

--- SRAstats.R
+++ SRAstats.R
@@ -1,6 +1,6 @@
 options(ENTREZ_KEY = "a65e721e6610fc345921a5a2778178171707")
 
-needed.packages = c('ggplot2', 'plotly', 'magrittr', 'dplyr', 'rstudioapi', 'taxize', 'phangorn', 'htmlwidgets','BiocManager')
+needed.packages = c('ggplot2', 'plotly', 'magrittr', 'dplyr', 'rstudioapi', 'taxize', 'phangorn', 'htmlwidgets','BiocManager','stringr')
 absent.packages = needed.packages[!(needed.packages %in% rownames(installed.packages()))]
 install.packages(absent.packages)
 
@@ -16,8 +16,8 @@ for(pack in needed.packages.bio) eval(parse(text = sprintf('library(%s)', pack))
 script.dir = dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(script.dir)
 
-run.info.file.name = 'Acari.csv'
-descr = 'Acari'
+run.info.file.name = 'insecta.csv'
+descr = 'Insecta'
 
 run.info.table = read.delim2(run.info.file.name, sep = ',', quote = '\"', check.names = FALSE, as.is = TRUE) %>% as.data.frame
 run.info.table$ReleaseDate = as.Date(run.info.table$ReleaseDate)
@@ -76,7 +76,8 @@ ggsave(file.name, gx, device="png", width = 12, height = 8, scale = 1.0)
 gx.kd = ggplot(run.info.table, aes(x = ReleaseDate, y = bases)) + 
   stat_density_2d(aes(fill = ..density.. ** 0.33), geom = "raster", contour = FALSE) +
   #stat_density_2d(geom = "polygon", aes(alpha = (..level..) ^ 2, fill = Label)) + 
-  scale_fill_distiller(palette=4, direction=1) +
+  scale_fill_distiller(palette='Spectral', direction=-1) +
+  #scale_fill_gradient2(low = "white", mid = "#9ae002", high = "#e05702", midpoint = 0.04) +
   geom_hline(yintercept = c_breaks, linetype="dashed", color = "grey", size=0.5) +
   theme(plot.title = element_text(size = 12, face = "bold", hjust = 0.5),
         text = element_text(size = 12),
@@ -88,9 +89,10 @@ gx.kd = ggplot(run.info.table, aes(x = ReleaseDate, y = bases)) +
   scale_y_continuous(name = "Число прочитанных нукл.", trans = 'log10', breaks = c_breaks) +
   scale_x_date(date_labels = "%Y", date_breaks = "1 year")
 
+
 file.name = sprintf('%s - timeline density.png', descr)
 #file.name.HTML = sprintf('%s - platform distrib.html', descr)
-ggsave(file.name, gx.kd, device="png", width = 12, height = 8, scale = 1.0)
+ggsave(file.name, gx.kd, device="png", width = 12, height = 8, scale = 0.7)
 #saveWidget(as_widget(ggplotly(g1)), file.name.HTML, selfcontained = TRUE)
 
 ###########################################################################################
@@ -136,8 +138,61 @@ gx = ggplot(df.aggr.ext, aes(x = ReleaseDate, y = bases)) +
 
 file.name = sprintf('%s - timeline2 - BioProjects.png', descr)
 file.name.HTML = sprintf('%s - timeline2 - BioProjects.html', descr)
-ggsave(file.name, gx, device="png", width = 12, height = 8, scale = 1.0)
+ggsave(file.name, gx, device="png", width = 12, height = 8, scale = 0.7)
 saveWidget(as_widget(ggplotly(gx)), file.name.HTML, selfcontained = TRUE)
+
+
+#########################################################################################
+##########################################################################################
+
+
+#'ReleaseDate'
+df = cbind(run.info.table[,c('bases'), drop = FALSE], as.data.frame(rep(1, nrow(run.info.table))))
+colnames(df) = c('bases', 'runs_count')
+df.aggr = aggregate.data.frame(x = df, by = list(BioProject.and.LibrarySource = sprintf('%s---%s', run.info.table$BioProject, run.info.table$LibrarySource)),  FUN = function(x){ sum (x) })
+
+df.aggr.part = t(as.data.frame(strsplit(df.aggr$BioProject.and.LibrarySource, '---')))
+colnames(df.aggr.part) = c('BioProject', 'LibrarySource')
+
+df.aggr = cbind(df.aggr, df.aggr.part)
+rownames(df.aggr) = df.aggr$BioProject.and.LibrarySource
+
+all.ReleaseDates = run.info.table$ReleaseDate
+names(all.ReleaseDates) = run.info.table$BioProject
+all.ReleaseDates.nr = all.ReleaseDates[!duplicated(names(all.ReleaseDates))]
+
+all.ReleaseDates.nr.df = as.data.frame(all.ReleaseDates.nr[df.aggr$BioProject])
+rownames(all.ReleaseDates.nr.df) = df.aggr$BioProject.and.LibrarySource
+df.aggr.ext = cbind(df.aggr, all.ReleaseDates.nr.df)
+colnames(df.aggr.ext) = c('BioProject.and.LibrarySource', 'bases', 'runs_count', 'BioProject', 'LibrarySource', 'ReleaseDate')
+
+df.aggr.ext$ReleaseDate = as.Date(df.aggr.ext$ReleaseDate)
+df.aggr.ext$log10_runs_count = log10(df.aggr.ext$runs_count)
+
+df.aggr.ext$BioProject[which.max(df.aggr.ext$bases)]
+
+max_log10 = ceiling(log10(sum(run.info.table$bases)))
+c_breaks = 10^(5:(max_log10 - 1))
+gx = ggplot(df.aggr.ext, aes(x = ReleaseDate, y = bases)) + 
+  geom_hline(yintercept = c_breaks, linetype="dashed", color = "grey", size=0.5) +
+  geom_point(alpha = 0.1, aes(size = log10_runs_count, color = LibrarySource)) +
+  theme(plot.title = element_text(size = 12, face = "bold", hjust = 0.5),
+        text = element_text(size = 12),
+        axis.title = element_text(face="bold"),
+        axis.text.x=element_text(size = 11, angle = 45, hjust = 1.0, vjust = 1.0),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black")) +
+  scale_y_continuous(name = "Total bases", trans = 'log10', breaks = c_breaks, limits = c(1e+6, 2e+14)) +
+  scale_x_date(date_labels = "%Y", date_breaks = "1 year")
+
+file.name = sprintf('%s - timeline2 - BioProjects ext.png', descr)
+file.name.HTML = sprintf('%s - timeline2 - BioProjects ext.html', descr)
+ggsave(file.name, gx, device="png", width = 12, height = 8, scale = 0.7)
+saveWidget(as_widget(ggplotly(gx)), file.name.HTML, selfcontained = TRUE)
+
+
+###########################################################################################
+###########################################################################################
 
 
 
@@ -451,29 +506,95 @@ dev.off()
 
 
 
+######################################################################################################
+######################################################################################################
+######################################################################################################
 
-#ggtree(as.phylo(tr$phylo))
+keep = run.info.table$Platform %in% c('OXFORD_NANOPORE', 'PACBIO_SMRT')
+keep = rep(TRUE, nrow(run.info.table))
+cat(sprintf('%d of %d datasets are done with Nanopore of PacBio\n', sum(keep), length(keep)))
 
-#nwk <- system.file("extdata", "sample.nwk", package="ggtree")
-#tree <- read.tree(nwk)
+run.info.table..lr = run.info.table[keep,,drop = FALSE]
 
-# 
-# 
-# 
-# run.info.table$TaxID
-# 
-# ggplot(as.data.frame(run.info.table), aes(x = Gene, y = Fold_change, fill = Stage)) +
-#   geom_boxplot(width=1.0, outlier.alpha = 0.3) +
-#   scale_fill_brewer(palette = 'RdBu') +
-#   theme(plot.title = element_text(size = 12, face = "bold", hjust = 0.5),
-#         text = element_text(size = 12),
-#         axis.title = element_text(face="bold"),
-#         axis.text.x=element_text(size = 11, angle = 45, hjust = 1, vjust = 0.5),
-#         axis.text.y=element_text(size = 11),
-#         panel.background = element_blank(),
-#         axis.line = element_line(colour = "black")) +
-#   ggtitle('') + 
-#   scale_y_continuous(name = "Relative expression level", trans = 'log10') +
-#   geom_hline(yintercept = 0, linetype="dotted", color = "blue", size=0.7)
-# 
-# ggplot()
+df..lr = cbind(run.info.table..lr[,c('bases'), drop = FALSE], as.data.frame(rep(1, nrow(run.info.table..lr))))
+colnames(df..lr) = c('bases', 'runs_count')
+df.aggr..lr = aggregate.data.frame(x = df..lr, by = list(BioProject.and.LibrarySource = sprintf('%s---%s', run.info.table..lr$BioProject, run.info.table..lr$LibrarySource)),  FUN = function(x){ sum (x) })
+
+df.aggr.part..lr = t(as.data.frame(strsplit(df.aggr..lr$BioProject.and.LibrarySource, '---')))
+colnames(df.aggr.part..lr) = c('BioProject', 'LibrarySource')
+
+df.aggr..lr = cbind(df.aggr..lr, df.aggr.part..lr)
+rownames(df.aggr..lr) = df.aggr..lr$BioProject.and.LibrarySource
+colnames(df.aggr..lr) = c('BioProject.and.LibrarySource', 'bases', 'runs_count', 'BioProject', 'LibrarySource')
+
+df.aggr.ext..lr = df.aggr..lr
+
+for(field in c('ReleaseDate', 'CenterName')){
+  all.data = run.info.table..lr[,field]
+  names(all.data) = run.info.table..lr$BioProject
+  all.data.nr = all.data[!duplicated(names(all.data))]
+  all.data.nr.df = as.data.frame(all.data.nr[df.aggr..lr$BioProject])
+  rownames(all.data.nr.df) = df.aggr..lr$BioProject.and.LibrarySource
+  df.aggr.ext..lr = cbind(df.aggr.ext..lr, all.data.nr.df)
+  colnames(df.aggr.ext..lr)[ncol(df.aggr.ext..lr)] = field
+}
+
+sel = which(sapply(df.aggr.ext..lr$CenterName, function(x) nchar(x) > 8))
+df.aggr.ext..lr$CenterName[sel] = str_to_title(df.aggr.ext..lr$CenterName)[sel]
+
+
+
+
+temp.aggr = aggregate.data.frame(x = run.info.table..lr[,c('Platform'), drop = FALSE],
+                                 by = list(BioProject.and.LibrarySource = sprintf('%s---%s', run.info.table..lr$BioProject, run.info.table..lr$LibrarySource)),
+                                 FUN = function(x){ 
+                                   c_text = paste(levels(as.factor(x)), collapse = '; ')
+                                   c_text = gsub('PACBIO_SMRT', 'PacBio', c_text, fixed = TRUE)
+                                   c_text = gsub('OXFORD_NANOPORE', 'Nanopore', c_text, fixed = TRUE)
+                                   c_text = gsub('ILLUMINA', 'Illumina', c_text, fixed = TRUE)
+                                   c_text = gsub('BGISEQ', 'BGI', c_text, fixed = TRUE)
+                                   c_text = gsub('ION_TORRENT', 'IonTorrent', c_text, fixed = TRUE)
+                                })
+rownames(temp.aggr) = temp.aggr$BioProject.and.LibrarySource
+df.aggr.ext..lr = cbind(df.aggr.ext..lr, as.data.frame(temp.aggr[rownames(df.aggr.ext..lr), 'Platform']))
+colnames(df.aggr.ext..lr)[length(df.aggr.ext..lr)] = 'Platforms'
+
+
+
+temp.aggr = aggregate.data.frame(x = run.info.table..lr[,c('ScientificName'), drop = FALSE],
+                                 by = list(BioProject.and.LibrarySource = sprintf('%s---%s', run.info.table..lr$BioProject, run.info.table..lr$LibrarySource)),
+                                 FUN = function(x){ length(levels(as.factor(x))) })
+rownames(temp.aggr) = temp.aggr$BioProject.and.LibrarySource
+df.aggr.ext..lr = cbind(df.aggr.ext..lr, as.data.frame(temp.aggr[rownames(df.aggr.ext..lr), 'ScientificName']))
+colnames(df.aggr.ext..lr)[length(df.aggr.ext..lr)] = 'N taxons'
+
+
+temp.aggr = aggregate.data.frame(x = run.info.table..lr[,c('ScientificName'), drop = FALSE],
+                                 by = list(BioProject.and.LibrarySource = sprintf('%s---%s', run.info.table..lr$BioProject, run.info.table..lr$LibrarySource)),
+                                 FUN = function(c_taxons){ 
+                                   c_table = table(c_taxons)
+                                   c_table = c_table[rev(order(c_table))]
+                                   c_top.taxons.info = sprintf('%s (%d)', names(c_table), c_table)
+                                   
+                                   if(length(c_table) > 3){
+                                     paste(c(c_top.taxons.info[1:3],'....'), collapse = '; ')
+                                   } else {
+                                     paste(c_top.taxons.info, collapse = '; ')
+                                   }
+                                 })
+
+
+rownames(temp.aggr) = temp.aggr$BioProject.and.LibrarySource
+df.aggr.ext..lr = cbind(df.aggr.ext..lr, as.data.frame(temp.aggr[rownames(df.aggr.ext..lr), 'ScientificName']))
+colnames(df.aggr.ext..lr)[length(df.aggr.ext..lr)] = 'Taxons'
+df.aggr.ext..lr$ReleaseDate = as.Date(df.aggr.ext..lr$ReleaseDate)
+
+omit = df.aggr.ext..lr$BioProject %in% ""
+omit = omit | (df.aggr.ext..lr$bases == 0)
+df.aggr.ext..lr = df.aggr.ext..lr[!omit,,drop = FALSE]
+df.aggr.ext..lr = df.aggr.ext..lr[rev(order(df.aggr.ext..lr$bases)),]
+
+write.table(df.aggr.ext..lr, sprintf('BioProject-centric.info.%s.tsv', descr), sep = '\t', quote = FALSE)
+
+
+
